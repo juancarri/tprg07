ba2025c0cec1fc07a3ca5182def77f7899acbb5a
GIO <grondan93@gmail.com>
2015-08-16 00:25:44 -0300

    s

--- tprog07/src/logica/ControladorCliente.java
+++ tprog07/src/logica/ControladorCliente.java
@@ -17,10 +17,17 @@ public class ControladorCliente {
     public void reservarPromocion(String proveedor, String promo, int cantidad, Date fechaIni, Date fechaFin){}
     public void altaCliente(String nick, String nombre, String apellido, String email, Date fechaNac, String imagen){}
     public Set<DataCliente> listarClientes(){
+        return null;
     }
     public void reservarServicio(String proveedor, String promo, int cantidad, Date fechaIni, Date fechaFin){}
     public void cancelarReserva(String nomCliente, int id){}
-    public Set<DataReserva> listarReservasXCliente(String nomCliente){}
-    public DataInfoReserva verInfoReserva(int id){}
-    public DataInfoCliente verInfoCliente(String nomCliente){}
+    public Set<DataReserva> listarReservasXCliente(String nomCliente){
+        return null;
+    }
+    public DataInfoReserva verInfoReserva(int id){
+        return null;
+    }
+    public DataInfoCliente verInfoCliente(String nomCliente){
+        return null;
+    }
 }
