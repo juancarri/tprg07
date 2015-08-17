500f9b79066dd48dc9966d0b84b660c07db71210
rolinares <r93linares@gmail.com>
2015-08-17 20:55:16 -0300

    Categoria, Ciudady Empresa

--- tprog07/src/logica/Categoria.java
+++ tprog07/src/logica/Categoria.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package logica;
+
+import java.util.Set;
+
 /**
  *
  * @author Juan
@@ -11,6 +14,54 @@ package logica;
 
 
 public class Categoria {
-    private
-            String nombre;
+    
+    private String nombre;
+    private Set<Categoria> hijos;
+    private Set<Servicio> servicios;   
+    
+    public Categoria(String nombre){
+        this.nombre = nombre;
+        this.hijos = null;
+        this.servicios = null;
+    }
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public Set<Categoria> getHijos(){
+        return this.hijos;
+    }
+    
+    public void setHijo(Categoria hijo){
+        this.hijos.add(hijo);
+    }
+    
+    public Set<Servicio> getServicios(){
+        return this.servicios;
+    }
+    
+    public void setServicio(Servicio servicio){
+        this.servicios.add(servicio);
+    }
+    
+    /**
+     *
+     * @param serv agrega este servicio a la coleccion de servicios de la categoria 
+     */
+    public void asociarServicioCategoria(Servicio serv){
+    
+    }
+    
+    /**
+     *
+     * @param cat es la categoria padre a la cual hay que agregarlo a sus hijos.
+     */
+    public void asociarPadre(Categoria cat){
+    
+    }
 }
